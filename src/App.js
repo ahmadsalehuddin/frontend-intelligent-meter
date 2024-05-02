936373fd9c096face95705ea8a02906eb20512bf
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2024-05-02 22:13:14 +0800

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Inventory from './inventory/Inventory';
+
+function App() {
+  return (
+    <Inventory />
+  );
+}
+
+export default App;
