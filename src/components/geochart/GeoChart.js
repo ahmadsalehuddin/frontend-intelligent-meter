b53ef5fc4a2624738a202406b9d6ba6b11c5d3b0
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2024-06-06 09:37:48 +0800

    relocate geojson files; add fetchdata function in geochart component

--- src/components/geochart/GeoChart.js
+++ src/components/geochart/GeoChart.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import { topojson } from "chartjs-chart-geo";
+
 export default function GeoChart() {
+  const [data, setData] = useState();
+  const [selangor, setSelangor] = useState();
+  const [districts, setDistricts] = useState();
 
+  const fetchData = async () => {
+    const result = await fetch("http://localhost:3000/selangor.json");
+    return await result.json();
+  };
 
+  useEffect(() => {
+    fetchData().then(value => setData(value));
+  }, []);
+
+  console.log(data.type);
   return <div>This is a geochart</div>;
 }
